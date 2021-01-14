92b97fe894027bf7a2c6c7171c8ac7c9070e83a7
Damon Bogich <damonbogich@gmail.com>
2021-01-14 11:33:29 -0700

    mvp met

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Route} from 'react-router-dom';
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +17,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response, 'get response');
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +35,13 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/'>
+        <MovieList movies={movieList}/>
+      </Route>
+
+      <Route path='/movies/:id'>
+        <Movie movies={movieList}/>
+      </Route>
     </div>
   );
 }
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useParams, useRouteMatch, Route, NavLink } from "react-router-dom";
 import axios from 'axios';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
-  // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
+  const params = useParams();
+  console.log(params, 'params');
+  const id = params.id;
 
   useEffect(() => {
     axios
@@ -13,13 +15,15 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        console.log(response, 'response on movie component')
+        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
